19712fd9c1054c8992ca053eb0fc88de23b2d14e
Pranav24PW-25 <pkpranavirs@gmail.com>
2025-09-17 17:36:20 +0530

    calc1

---
+++ calculator.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter Number 1: "))
+num2 = float(input("Enter Number2: "))
+
+operation = input("Enter 1 for addition or 2 for subtraction: ")
+
+if(operation == 1):
+    print("Result: ",num1 + num2)
+
+elif(operation == 2):
+    print("Result: ",num1 - num2)
